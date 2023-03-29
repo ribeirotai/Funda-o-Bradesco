8d4dce58b5c6e9767377f098574215ecd2f3bceb
Tainá Ribeiro <107816654+ribeirotai@users.noreply.github.com>
2023-03-28 21:11:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener ('click', function() 
+{
+    document.body.classList.toggle('dark-theme')
+    
+    var className = document.body.className;
+    if (className == "ligth-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "ligth";
+    }
+});
